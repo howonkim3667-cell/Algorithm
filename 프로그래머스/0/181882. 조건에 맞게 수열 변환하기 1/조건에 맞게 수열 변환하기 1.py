7b2009f684e6cebc8d780870f71aac41bb914b15
Hogom <howon.kim3667@gmail.com>
2025-10-27 14:36:44 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 130.76 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    for i in arr:
+        if i<50 and i%2:
+            answer.append(i*2)
+        elif i>=50 and not i%2:
+            answer.append(int(i/2))
+        else:
+            answer.append(i)
+        
+    return answer
