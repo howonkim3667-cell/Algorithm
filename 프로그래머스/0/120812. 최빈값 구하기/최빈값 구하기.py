bceada7a38ce46fd363d58e3c404c49eb3451c02
Hogom <howon.kim3667@gmail.com>
2025-11-03 08:59:10 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.63 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,14 +1,14 @@
 def solution(array):
     a = sorted(list(set(array)))
     b = {}
-    print(a)
+
     if len(a) == 1:
         return a[0]
     for i in a:
         b[i] = array.count(i)
-    print(b)
+
     a = [k for k, v in b.items() if max(b.values())== v]
-    print(a)
+
     if len(a) >1:
         return -1
     return a[0]
