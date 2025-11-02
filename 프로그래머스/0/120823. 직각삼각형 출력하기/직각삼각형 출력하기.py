8a6881e1dbed4668a6a4febdafcfdfa8c2ae6a8c
Hogom <howon.kim3667@gmail.com>
2025-11-02 18:16:28 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 23.09 ms, Memory: 7.41 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.py
@@ -0,0 +1,11 @@
+# n = input()
+# for i in range(1, int(n)+1):
+#     for j in range(1, i+1):
+#         print('*'*j, sep='',end='')
+#     print()
+
+n = input()
+for i in range(1, int(n)+1):
+    print('*'*i)
+        
+    
