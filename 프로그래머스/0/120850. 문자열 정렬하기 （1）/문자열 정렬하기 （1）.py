f465aa19c64e311048e6c071cc54f92517d0aba2
Hogom <howon.kim3667@gmail.com>
2025-11-02 19:04:48 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -1,6 +1,3 @@
 def solution(my_string):
-    a = []
-    for i in my_string:
-        if i.isnumeric():
-            a.append(int(i))
-    return sorted(a)
+
+    return sorted([int(i) for i in my_string if i.isnumeric()])
