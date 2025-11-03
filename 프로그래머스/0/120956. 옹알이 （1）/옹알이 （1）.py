53cc8e748b2a8d0714a0597b045fa00df545d111
Hogom <howon.kim3667@gmail.com>
2025-11-03 11:36:18 +0900

    [level 0] Title: 옹알이 (1), Time: 0.15 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,16 +1,35 @@
+# def solution(babbling):
+#     a =["aya", "ye", "woo", "ma"]
+#     c =0
+#     for i in a:
+#         for j in babbling:
+#             if i in j:
+#                 c+=1
+#     return c
+
 def solution(babbling):
-    a =["aya", "ye", "woo", "ma"]
-    combi =[]
-    count = 0
-    for i in range(len(a)):
-        for j in range(len(a)):
-            combi.append(a[i]+a[j])
-            for z in range(len(a)):
-                combi.append(a[i]+a[j]+a[z])
-                for b in range(len(a)):
-                    combi.append(a[i]+a[j]+a[z]+a[b])
-    combi +=a
-    for i in babbling:
-        if i in combi:
-            count +=1
-    return count
+    c = 0
+    for b in babbling:
+        for w in [ "aya", "ye", "woo", "ma" ]:
+                b = b.replace(w, ' ')
+        if len(b.strip()) == 0:
+            c += 1
+    return c
+
+
+# def solution(babbling):
+#     a =["aya", "ye", "woo", "ma"]
+#     combi =[]
+#     count = 0
+#     for i in range(len(a)):
+#         for j in range(len(a)):
+#             combi.append(a[i]+a[j])
+#             for z in range(len(a)):
+#                 combi.append(a[i]+a[j]+a[z])
+#                 for b in range(len(a)):
+#                     combi.append(a[i]+a[j]+a[z]+a[b])
+#     combi +=a
+#     for i in babbling:
+#         if i in combi:
+#             count +=1
+#     return count
