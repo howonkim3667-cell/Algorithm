5fe2faeb8a6a1ccb603c7174d6627668e72f0c07
Hogom <howon.kim3667@gmail.com>
2025-11-08 18:01:00 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,31 @@
+def solution(numer1, denom1, numer2, denom2):
+    # 분수의 합 answer[0]분자 answer[1] 분모
+    answer = [numer1*denom2 + numer2*denom1 ,  denom1*denom2]
+    
+    # 분모와 분자의 최대공약수
+    a , b = answer[0], answer[1]
+    while b !=0:
+        temp = a
+        a = b
+        b = temp % b
+        
+    answer[0] = answer[0]//a
+    answer[1] = answer[1]//a
+    
+    return answer
+        
+    
+    
+#     answer.append((numer1 / denom1) + (numer2, denom2))
+#     return answer
+
+# def gcd(a, b):
+#     while b != 0:
+#         a, b = b, a % b
+#     return a
+
+# def solution(numer1, denom1, numer2, denom2):
+#     answer = [numer1 * denom2 + numer2 * denom1, denom1 * denom2]
+#     GCD = gcd(answer[0], answer[1])
+#     answer[0], answer[1] = answer[0] // GCD, answer[1] // GCD
+#     return answer
