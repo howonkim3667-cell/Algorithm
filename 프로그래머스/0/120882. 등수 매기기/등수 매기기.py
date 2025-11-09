5cc725975524c501331390b71e7f8bd934ef6f6f
Hogom <howon.kim3667@gmail.com>
2025-11-09 20:35:03 +0900

    [level 0] Title: 등수 매기기, Time: 0.03 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.py
@@ -1,12 +1,26 @@
-def mean(a):
-    return (a[0]+a[1])/len(a)
-def solution(score):
-    for i in range(len(score)):
-        score[i] = mean(score[i])
+# def mean(a):
+#     return (a[0]+a[1])/len(a)
+
+# def solution(score):
+#     for i in range(len(score)):
+#         score[i] = mean(score[i])
         
-    s_score = sorted(score, reverse = True)
+#     s_score = sorted(score, reverse = True)
     
-    answer = []
-    for i in score:
-        answer.append(s_score.index(i)+1)
-    return answer
+#     answer = []
+#     for i in score:
+#         answer.append(s_score.index(i)+1)
+#     return answer
+
+def solution(score):
+    mean_score = [sum(i)/len(i)for i in score]
+    s_score = sorted([sum(i)/len(i)for i in score], reverse =True)
+    
+    
+    print(s_score)
+    dict = {}
+    for i, v in enumerate(s_score):
+        if v not in dict.keys():
+            dict[v] = i+1
+    print(dict)
+    return [dict[i] for i in mean_score]
