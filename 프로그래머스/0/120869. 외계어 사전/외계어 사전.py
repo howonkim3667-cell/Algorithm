ab2ac5fa92989f3d3db6d31922861dfbcf3aaee7
Hogom <howon.kim3667@gmail.com>
2025-11-09 19:42:39 +0900

    [level 0] Title: 외계어 사전, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -0,0 +1,15 @@
+def solution(spell, dic):
+    answer= []
+    for i in dic:
+        state = True
+        for j in spell:
+            if j not in i:
+                state =False
+                break
+        if state:
+            answer.append(i)
+    if len(answer) == 0:
+        return 2
+    return 1
+    print(answer)
+    
